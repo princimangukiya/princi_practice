dd0b5088bbf9fc301cf22a599fc046e089f52f4a
Parth Kansagra <66050419+Parth7656@users.noreply.github.com>
2021-06-23 13:36:47 +0530

    0.0.3
    add,update,delete supplier now working

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -3,26 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\d_purchase;
+use App\Models\supplier_details;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
 
-class DiamondController extends Controller
+class SupplierController extends Controller
 {
     public function index()
     {
         $data = array();
-        $data['diamond'] = d_purchase::get();
-        return view('Diamond_purchase.index',$data);
+        $data['supplier'] = supplier_details::get();
+        return view('supplier_details.index',$data);
     }
 
     public function create()
     {
-        return view('Diamond_purchase.create');
+        return view('supplier_details.create');
     }
 
     public function store(Request $request)
@@ -30,15 +30,9 @@ class DiamondController extends Controller
         
         try {
             $validator = Validator::make($request->all(), [
-                'bar_code' => 'required|unique:d_purchase,d_barcode',
-                'packatename' => 'required',
-                'd_wt' => 'required',
-                'd_col' => 'required',
-                'd_pc' => 'required',
-                'd_shape' => 'required',
-                'd_cla' => 'required',
-                'd_exp_pr' => 'required',
-                'd_exp' =>  'required',
+                's_name' => 'required',
+                's_address' => 'required',
+                's_gst' => 'required|unique:supplier_details,s_gst',
 
             ]);
             //dd($request);
@@ -46,18 +40,13 @@ class DiamondController extends Controller
                 return redirect()->back()->withErrors($validator)->withInput($request->all());
             }
 
-            $newitem = new d_purchase();
-            $newitem->d_barcode = !empty($request->bar_code) ? $request->bar_code : '';
-            $newitem->d_wt = !empty($request->d_wt) ? $request->d_wt : '';
-            $newitem->d_col = !empty($request->d_col) ? $request->d_col : '';
-            $newitem->d_pc = !empty($request->d_pc) ? $request->d_pc : '';
-            $newitem->d_shape = !empty($request->d_shape) ? $request->d_shape : '';
-            $newitem->d_cla = !empty($request->d_cla) ? $request->d_cla : '';
-            $newitem->d_exp_pr = !empty($request->d_exp_pr) ? $request->d_exp_pr : '';
-            $newitem->d_exp = !empty($request->d_exp) ? $request->d_exp : '';
+            $newitem = new supplier_details();
+            $newitem->s_name = !empty($request->s_name) ? $request->s_name : '';
+            $newitem->s_address = !empty($request->s_address) ? $request->s_address : '';
+            $newitem->s_gst = !empty($request->s_gst) ? $request->s_gst : '';
             $newitem->save();
 
-            return Redirect::to('/diamond')->with($notification);
+            return Redirect::to('/supplier')->with($notification);
 /*
             $parent = 0;
             if ($request->refer_code != null) {
@@ -167,7 +156,66 @@ class DiamondController extends Controller
                 'alert-type' => 'success'
             );
 
-            return Redirect::to('/diamond')->with($notification);
+            return Redirect::to('/supplier')->with($notification);
         }
+    }
+
+    // supplier edit
+    public function edit($id)
+    {
+        //dd($id);
+        $data = array();
+        $data['supplier']=supplier_details::findOrFail($id);
+        return view('supplier_details.edit', $data);
+    }
+
+    // user update
+    public function update(Request $request, $id)
+    {
+
+        try {
+            $validator = Validator::make($request->all(), [
+                's_name' => 'required',
+                's_address' => 'required',
+                's_gst' => 'required',
+
+            ]);
+            //dd($request);
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors($validator)->withInput($request->all());
+            }
+
+            $newitem = array();
+            $newitem['s_name'] = !empty($request->s_name) ? $request->s_name : '';
+            $newitem['s_address'] = !empty($request->s_address) ? $request->s_address : '';
+            $newitem['s_gst'] = !empty($request->s_gst) ? $request->s_gst : '';
+
+            supplier_details::where('s_id',$id)->update($newitem);
+
+            return Redirect::to('/supplier')->with($notification);
+        
+        } catch (\Throwable $th) {
+            $notification = array(
+                'message' => 'User can`t Update!',
+                'alert-type' => 'error'
+            );
+
+            return Redirect::to('/supplier')->with($notification);
+        }
+    }
+
+    // supplier delete
+    public function destroy($id)
+    {
+        //
+        $suplier = supplier_details::find($id);
+        $suplier->delete();
+        $notification = array(
+            'message' => 'User Deleted!',
+            'alert-type' => 'success'
+        );
+
+        return Redirect::to('/supplier')->with($notification);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers;
+use App\Http\Controllers as Controllers;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,7 +22,15 @@ Route::get('/', function () {
 Route::get('/check', function () {
     return view('check');
 });
-
+//diamond purchase
 Route::get('/diamond','App\Http\Controllers\DiamondController@index')->name('diamond');
 Route::get('/diamond/create','App\Http\Controllers\DiamondController@create')->name('diamond.create');
 Route::post('/diamond/store','App\Http\Controllers\DiamondController@store')->name('diamond.store');
+
+//supplier
+Route::get('/supplier','App\Http\Controllers\SupplierController@index')->name('supplier');
+Route::get('/supplier/create','App\Http\Controllers\SupplierController@create')->name('supplier.create');
+Route::post('/supplier/store','App\Http\Controllers\SupplierController@store')->name('supplier.store');
+Route::get('/supplier/edit/{id}','App\Http\Controllers\SupplierController@edit')->name('supplier.edit');
+Route::post('/supplier/update/{id}','App\Http\Controllers\SupplierController@update')->name('supplier.update');
+Route::post('/supplier/{id}/destroy','App\Http\Controllers\SupplierController@destroy')->name('supplier.destroy');
