f7dddfa20426e6ea3f41a26a25d243c975c9e209
Brijesh Goyani <you@example.com>
2021-07-02 13:11:14 +0530

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-	
-<!-- Mirrored from laravel.spruko.com/admitro/Vertical-IconSidedar-Light/index by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 14 Apr 2021 10:22:47 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
 		<!-- Meta data -->
 		<meta charset="UTF-8">
 		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
-		<meta content="Admitro - Laravel Bootstrap Admin Template" name="description">
-		<meta content="Spruko Technologies Private Limited" name="author">
+		<meta content="VM Jewels Billing and Inventory Management System" name="description">
+		<meta content="Webito Infotech Private Limited" name="author">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<meta name="keywords" content="laravel admin dashboard, best laravel admin panel, laravel admin dashboard, php admin panel template, blade template in laravel, laravel dashboard template, laravel template bootstrap, laravel simple admin panel,laravel dashboard template,laravel bootstrap 4 template, best admin panel for laravel,laravel admin panel template, laravel admin dashboard template, laravel bootstrap admin template, laravel admin template bootstrap 4"/>
 
@@ -332,8 +330,5 @@
 		
 		<script src="{{asset('T3_Admin_Design/media/js/jquery.dataTables.min.js')}}"></script>
 
-    
-
-<!-- Mirrored from laravel.spruko.com/admitro/Vertical-IconSidedar-Light/index by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 14 Apr 2021 10:24:36 GMT -->
-</html>
+    </html>
 		
