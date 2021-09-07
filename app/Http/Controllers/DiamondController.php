77e1d917d1062d6b21b7ffb2998719c856084c08
Rushikesh Antala <86461351+RushikeshAntala@users.noreply.github.com>
2021-09-07 15:27:47 +0530

    edit and delete function added

--- app/Http/Controllers/DiamondController.php
+++ app/Http/Controllers/DiamondController.php
@@ -18,17 +18,23 @@ class DiamondController extends Controller
 {
     public function index()
     {
-        $c_id = session()->get('c_id');
+       
         $data = array();
+        $c_id = session()->get('c_id');
         $data['diamond'] = D_Purchase::where('c_id', $c_id)->with('shapeDate', 'supplier')->get();
         return view('Diamond_purchase.index', $data);
     }
 
     public function create()
     {
-        $c_id = session()->get('c_id');
+        
         $data = array();
+        $c_id = session()->get('c_id');
+        // $data['supplier'] = D_Purchase::where('c_id', $c_id)->get();
         $data['supplier'] = Supplier_Details::where('c_id', $c_id)->get();
+        $data['supplier']= D_Purchase::join('supplier_details', 'D_Purchase.s_id', '=', 'supplier_details.s_id')
+        ->join('diamond_shape', 'd_purchase.shape_id', '=', 'diamond_shape.shape_id')
+        ->get(['D_Purchase.*', 'supplier_details.*', 'diamond_shape.*']);
         return view('Diamond_purchase.create', $data);
     }
 
@@ -71,5 +77,56 @@ class DiamondController extends Controller
         } catch (\Throwable $th) {
             return Response::json(array('success' => false));
         }
+    }
+    public function edit($id)
+    {
+        //dd($id);
+        $data = array();
+        $data['supplier'] = D_Purchase::findOrFail($id);
+        return view('Diamond_Purchase.edit', $data);
+    }
+    public function update(Request $request, $id)
+    {
+
+        // try {
+        //     $validator = Validator::make($request->all(), [
+        //         's_name' => 'required',
+        //         's_address' => 'required',
+        //         's_gst' => 'required|unique:supplier_details,s_gst',
+
+        //     ]);
+        //     //dd($request);
+        //     if ($validator->fails()) {
+        //         return redirect()->back()->withErrors($validator)->withInput($request->all());
+        //     }
+
+        $c_id = session()->get('c_id');
+        $newitem = new D_Purchase();
+        $newitem->d_barcode = !empty($request->bar_code) ? $request->bar_code : '';
+        $newitem->d_wt = !empty($request->d_wt) ? $request->d_wt : '';
+        $newitem->s_id = !empty($request->s_id) ? $request->s_id : '';
+        $newitem->c_id = $c_id;
+        //$newitem->d_col = !empty($request->d_col) ? $request->d_col : '';
+        //$newitem->d_pc = !empty($request->d_pc) ? $request->d_pc : '';
+        $newitem->shape_id = !empty($request->shape_id) ? $request->shape_id : '';
+        //$newitem->d_cla = !empty($request->d_cla) ? $request->d_cla : '';
+        //$newitem->d_exp_pr = !empty($request->d_exp_pr) ? $request->d_exp_pr : '';
+        //$newitem->d_exp = !empty($request->d_exp) ? $request->d_exp : '';
+        D_Purchase::where('d_id', $id)->update($newitem);
+
+        return Redirect::to('/diamond/create');
+        // }
+    }
+    public function destroy($id)
+    {
+        //
+        $dimond = D_Purchase::find($id);
+        $dimond->delete();
+        $notification = array(
+            'message' => 'User Deleted!',
+            'alert-type' => 'success'
+        );
+
+        return Redirect::to('/diamond')->with($notification);
     }
 }
