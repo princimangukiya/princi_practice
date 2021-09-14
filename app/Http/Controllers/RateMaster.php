e9938482f6bd5132fed481b8ffe9a79096e570f2
Rushikesh Antala <86461351+RushikeshAntala@users.noreply.github.com>
2021-09-14 19:01:26 +0530

    Add Json thrue Data

--- app/Http/Controllers/RateMaster.php
+++ app/Http/Controllers/RateMaster.php
@@ -31,78 +31,82 @@ class RateMaster extends Controller
     }
     public function store(Request $request)
     {
-
-        // try {
-        //     $validator = Validator::make($request->all(), [
-        //         'Rate' => 'rate',
-        //         'price' => 'required|unique:supplier_details,price',
-
-        //     ]);
-        //     dd($request);
-        //     if ($validator->fails()) {
-        //         return redirect()->back()->withErrors($validator)->withInput($request->all());
-        //     }
-
-        //     if ($validator->fails()) {
-        //         return Response::json(array('success' => false));
-        //     }
-
-        //     $suppilerData = Supplier_Details::where('s_gst', $request->s_gst)->first();
-
-        //     if ($suppilerData == null) {
-        $s_id = session()->get('s_id');
-        $data['supplier'] = rate_master::where('s_id', $s_id)->get();
+        $r_id = $request->r_id;
+        // echo $r_id;
+        $price = $request->price;
+        $data1 = [$r_id => $price];
+        $firstTimeData = ["1"=>[$r_id => $price]];
+        // var_dump($data1);
+        
+        $c_id = session()->get('c_id');
+        // echo $c_id;
+        $s_id = $request->s_id;
+        $data = rate_master::where([['c_id', $c_id] , ['s_id', $s_id]])->get();
+        // echo $data;
+        // echo "-=======";
         $new_rate = new rate_master();
-      
-        $new_rate->s_id = !empty($request->s_id) ? $request->s_id : '';
-        $supplier_id = $request->s_id;
-        $data_s_id = $new_rate->s_id;
-        $data = $request->only('r_id', 'price');
-        // $data_json = rate_master::find($supplier_id)->get('rate_masters.json_price');
-        $data = $request->only('r_id', 'price');
-        
-        if ($supplier_id == $data_s_id) 
-        {
-            $r_id = rate_master::where('s_id', $supplier_id)->select('Rate_id')->get();
-            $json_data = rate_master::where('s_id', $supplier_id)->select('json_price')->get();
-            $json_data = json_decode($json_data);
-            array_push($json_data, $data);
-            $json_data = json_encode($json_data);
-            //        echo '<br>';
-            //    echo $json_data;
-            rate_master::where('r_id', $r_id)->update($json_data);
-            $new_rate->json_price = $json_data;
-
-        } 
-        
-        else {
-            $new_rate->s_id = !empty($request->s_id) ? $request->s_id : '';
-            $new_rate->json_price = json_encode($data);
-        }
-        echo "<br>";
-        echo "<br>";
-        // echo $newrate;
-        // $new_rate->save();
+       
+        // $arr = ["2"=>"100", "5"=>"400"];
+        // $arr2 = ["3"=>"500"];
+        // array_push($arr, $arr2);
+        // $bb = array_values($arr);
+        // echo json_encode($bb);
+    if($data->isEmpty())
+    {
+        echo "coming if";
+        $new_rate->c_id  = $c_id;
+        $new_rate->s_id = $s_id;
+        $arrValuesJson = array_values($firstTimeData);
+        $new_rate->json_price = json_encode($arrValuesJson);
+        $new_rate->save();
         return Redirect::to('/rate_master');
-        //         return Response::json(array('success' => true));
-        //     } else {
-        //         return Response::json(array('success' => 200));
-        //     }
-        // } catch (\Throwable $th) {
-        //     $notification = array(
-        //         'message' => 'Rate can`t Add!',
-        //         'alert-type' => 'success'
-        //     );
-        // return Redirect::to('/rate_master');
-        // return Response::json(array('success' => false));
-        // }
+    }      
+    else{
+        foreach($data as $rateData){
+        
+        $json_data= $rateData['json_price'];
+        // $json_data1 = json_decode($json_data , true);
+        // echo json_encode($json_data1);
+        $someArray = json_decode($json_data, true);
+        print_r($someArray);        // Dump all data of the Array
+        echo "<br>";
+        // $find_price = ;
+        // echo "===================<br>";
+        // echo $find_price;
+        if($someArray[0][$r_id]->isEmpty())
+        {
+            echo "Welcome If Condition";
+        }
+        else{
+            echo "Welcome else";
+            // var_dump($json_data1);
+            array_push($json_data1, $data1);
+            $arrValuesJson = array_values($json_data1);
+            $json_data2 = json_encode($arrValuesJson);
+            // var_dump($json_data1);
+            $new_rate->json_price = $json_data;
+            rate_master::where([['c_id', $c_id] , ['s_id', $s_id]])->update(['json_price' => $json_data2]);
+       
+        }
+        // var_dump($json_data1);
+        // array_push($json_data1, $data1);
+        // $arrValuesJson = array_values($json_data1);
+        // $json_data2 = json_encode($arrValuesJson);
+        // var_dump($json_data1);
+        // $new_rate->json_price = $json_data;
+        // rate_master::where([['c_id', $c_id] , ['s_id', $s_id]])->update(['json_price' => $json_data2]);
+       
+        }
+        return Redirect::to('/rate_master');
+    }  
+       
     }
     public function rates_store(Request $request)
     {
 
        
         $rate= new rate();
-        $rate->Rates = $request->Rates;
+        $rate->wt_category = $request->Rates;
         $rate->save();
         return Redirect::to('/rate_master/create');
     }
