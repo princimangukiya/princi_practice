ef178c46c788283418d92ec170de580b2bf7a552
Rushikesh Antala <86461351+RushikeshAntala@users.noreply.github.com>
2021-09-22 19:40:53 +0530

    change ui design

--- resources/views/Daimond_tracker/index.blade.php
+++ resources/views/Daimond_tracker/index.blade.php
@@ -5,19 +5,19 @@
 @section('content')
     <div class="page-header">
         <div class="page-leftheader">
-            <h4 class="page-title mb-0">Rate Master</h4>
+            <h4 class="page-title mb-0">Daimond Tracker</h4>
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="#"><i class="fe fe-layout mr-2 fs-14"></i>Other Features</a></li>
-                <li class="breadcrumb-item active" aria-current="page"><a href="#">Rate Master List</a></li>
+                <li class="breadcrumb-item active" aria-current="page"><a href="#">Daimond Tracker</a></li>
             </ol>
         </div>
-        <div class="page-rightheader">
+        {{-- <div class="page-rightheader">
             <div class="btn btn-list">
                 <a href="{{ route('rate_master.create') }}" class="btn btn-info"><i class="fa fa-user-plus mr-1"></i> Add
                     Rate </a>
 
             </div>
-        </div>
+        </div> --}}
     </div>
     <!--End Page header-->
     <!-- Row -->
@@ -26,7 +26,7 @@
             <!--div-->
             <div class="card">
                 <div class="card-header">
-                    <div class="card-title">Rate Details</div>
+                    <div class="card-title">Daimond Tracker Details</div>
                 </div>
                 <div class="card-body">
                     <div class="___class_+?17___">
@@ -37,59 +37,20 @@
                                         <th class="border-bottom-0">#</th>
                                         <th class="border-bottom-0">Company Name</th>
                                         <th class="border-bottom-0">Rate</th>
-                                        {{-- <th>Package</th> --}}
-                                        {{-- <th class="border-bottom-0">0.210-0.409</th>
-                                        <th class="border-bottom-0">0.410-5.000</th> --}}
-
                                         <th class="border-bottom-0">Price</th>
+                                        <th class="border-bottom-0">Buy_Date</th>
+                                        <th class="border-bottom-0">Sell_Date</th>
                                     </tr>
                                 </thead>
                                 {{-- <tbody>
-                                    @foreach ($supplier as $key => $value)
-                                        <tr>
-
-                                            <td>
-                                                {{ $key + 1 }}
-                                            </td>
-                                            <td>
-                                                {{ $value->s_id }}
-                                            </td>
-                                            <td>
-                                                {{ $value->Rate }}
-                                            </td>
-                                            {{-- <td>
-                                                {{ $value->Price }}
-                                            </td>
-                                            <td>
-                                                {{ $value->Price }}
-                                            </td> --}
-                                            <td>
-                                                {{ $value->Price }}
-                                            </td>
-                                            <td class="align-middle"
-                                                style="display: flex; align-items: center;justify-content: space-evenly;">
-                                                <a href="{{ route('rate_master.edit', ['id' => $value->Rate_id]) }}">
-                                                    <div class="btn-group align-top">
-                                                        <button class="btn btn-sm btn-success" type="button"
-                                                            data-toggle="modal" data-target="#user-form-modal">Edit</button>
-                                                        <button class="btn btn-sm btn-success" type="button"><i
-                                                                class="fe fe-edit-2"></i></button>
-                                                    </div>
-                                                </a>
-
-                                                <form action="{{ route('rate_master.destroy', $value->Rate_id) }}"
-                                                    method="post">
-                                                    @csrf
-                                                    <div class="btn-group align-top">
-                                                        <button class="btn btn-sm btn-danger">Delete</button>
-                                                        <button class="btn btn-sm btn-danger"><i
-                                                                class="fe fe-trash-2"></i></button>
-                                                    </div>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
                                 </tbody> --}}
                             </table>
                         </div>
