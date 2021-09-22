481b8c4613196e884a7046297e067ad8ffb0debd
Rushikesh Antala <86461351+RushikeshAntala@users.noreply.github.com>
2021-09-22 19:26:56 +0530

    dynmically added data For Donloade Pdf

--- resources/views/Report/Inward_Formatte.blade.php
+++ resources/views/Report/Inward_Formatte.blade.php
@@ -6,8 +6,8 @@
 </head>
 
 <body>
-    <h1>{{ $title }}</h1>
-    <p>{{ $date }}</p>
+    <h1>All InWard Data Report</h1>
+    <p>22/09/2021</p>
     <div class="row">
         <div class="col-12">
             <!--div-->
@@ -27,46 +27,35 @@
                                         <th class="border-bottom-0" style="border: 1px solid black">Shape</th>
                                         <th class="border-bottom-0" style="border: 1px solid black">Old_Weight</th>
                                         <th class="border-bottom-0" style="border: 1px solid black">New_Weight</th>
-                                        <th class="border-bottom-0" style="border: 1px solid black">Price</th>
                                         <th class="border-bottom-0" style="border: 1px solid black">Buy_date</th>
-                                        {{-- <th>Package</th> --}}
-                                        {{-- <th class="border-bottom-0">0.210-0.409</th>
-                                        <th class="border-bottom-0">0.410-5.000</th> --}}
-
-                                        <th class="border-bottom-0" style="border: 1px solid black">Sell_Date</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr style="border: 1px solid black">
-                                        <td style="border: 1px solid black">
-                                            1
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            ALOK IMPEX
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            1008894
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            એમરલ
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            0.194
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            0.180
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            23
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            2021-08-29
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            2021-09-05
-                                        </td>
-
-                                    </tr>
+                                    @foreach ($inward as $key => $value)
+                                        <tr style="border: 1px solid black">
+                                            <td style="border: 1px solid black">
+                                                {{ $key + 1 }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                {{ $value->s_name }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                {{ $value->d_barcode }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                {{ $value->shape_name }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                {{ $value->d_wt }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                {{ $value->d_n_wt }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                {{ date('d-m-Y', strtotime($value->bill_date)) }}
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
@@ -79,4 +68,5 @@
         </div>
     </div>
 </body>
+
 </html>
--- resources/views/Report/Outward_Formatte.blade.php
+++ resources/views/Report/Outward_Formatte.blade.php
@@ -6,8 +6,8 @@
 </head>
 
 <body>
-    <h1>{{ $title }}</h1>
-    <p>{{ $date }}</p>
+    <h1>All OutWard Data Report</h1>
+    <p>22/09/2021</p>
     <div class="row">
         <div class="col-12">
             <!--div-->
@@ -29,44 +29,54 @@
                                         <th class="border-bottom-0" style="border: 1px solid black">New_Weight</th>
                                         <th class="border-bottom-0" style="border: 1px solid black">Price</th>
                                         <th class="border-bottom-0" style="border: 1px solid black">Buy_date</th>
-                                        {{-- <th>Package</th> --}}
-                                        {{-- <th class="border-bottom-0">0.210-0.409</th>
-                                        <th class="border-bottom-0">0.410-5.000</th> --}}
-
                                         <th class="border-bottom-0" style="border: 1px solid black">Sell_Date</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr style="border: 1px solid black">
-                                        <td style="border: 1px solid black">
-                                            1
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            ALOK IMPEX
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            1008894
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            એમરલ
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            0.194
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            0.180
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            23
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            2021-08-29
-                                        </td>
-                                        <td style="border: 1px solid black">
-                                            2021-09-05
-                                        </td>
+                                    @foreach ($inward as $key => $value)
 
-                                    </tr>
+
+                                        <tr>
+                                            <td style="border: 1px solid black">
+                                                {{ $key + 1 }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                @php
+                                                    $s_id = $value->s_id;
+                                                    $s_name = App\Models\supplier_details::where('s_id', $s_id)->get('s_name');
+                                                    // echo $s_name;
+                                                @endphp
+                                                {{ $s_name[0]['s_name'] }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                {{ $value->d_barcode }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                @php
+                                                    $shape_id = $value->shape_id;
+                                                    $shape_name = App\Models\diamond_shape::where('shape_id', $shape_id)->get('shape_name');
+                                                    // echo $s_name;
+                                                @endphp
+                                                {{ $shape_name[0]['shape_name'] }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                {{ $value->d_wt }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                {{ $value->d_n_wt }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                {{ $value->price }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                {{ date('d-m-Y', strtotime($value->bill_date)) }}
+                                            </td>
+                                            <td style="border: 1px solid black">
+                                                {{ date('d-m-Y', strtotime($value->updated_at)) }}
+                                            </td>
+
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
