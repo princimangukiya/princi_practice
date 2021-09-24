d22028e466e5ba0866a42f6b247e75844946492d
Rushikesh Antala <86461351+RushikeshAntala@users.noreply.github.com>
2021-09-24 18:51:53 +0530

    change pdf formatte

--- resources/views/Report/Outward_Formatte.blade.php
+++ resources/views/Report/Outward_Formatte.blade.php
@@ -1,22 +1,67 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Outward Report</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <title>Inward</title>
+    <style>
+        @font-face {
+            font-family: myguj-font;
+            src: url(Gujrati-Saral-1.ttf) format(truetype);
+            font-weight: normal;
+            font-style: normal;
+        }
+
+        .myguj {
+            font-family: myguj-font;
+        }
+
+        .tabel_style {
+            text-align: center;
+            margin: 2% 2%;
+        }
+
+        .invoice {
+            width: 100%;
+            padding: 1%;
+            border: 1px solid black;
+        }
+
+        .textstyle {
+            align-items: center;
+        }
+
+    </style>
 </head>
 
-<body>
-    <h1>All OutWard Data Report</h1>
-    <p>22/09/2021</p>
-    <div class="row">
-        <div class="col-12">
+
+
+<body style="margin: 0; padding: 0; background-color:#eaeced " bgcolor="#eaeced">
+    <div class="row tabel_style">
+        <div class="col-12 invoice">
             <!--div-->
-            <div class="card">
-                <div class="card-header">
-                    <div class="card-title">Outward Details</div>
+            <div class="card1">
+                <div class="textstyle">
+                    <h6 class="myguj">|| શ્રી ગણેશાય નામ: ||</h6>
                 </div>
+                @php
+                    $c_id = session()->get('c_id');
+                    $rate = App\Models\supplier_details::where('c_id', $c_id)->get();
+                    // echo $rate;
+                @endphp
+                @foreach ($rate as $key => $value)
+                    @if ($value->s_id == 8)
+                        <h3>{{ $value->s_name }}</h3>
+                    @endif
+                @endforeach
+                <p>Date:- 23/9/2021</p>
+
                 <div class="card-body">
-                    <div style="border: 1px solid black">
+                    <div class="___class_+?17___">
                         <div class="table-responsive">
                             <table id="example" class="table table-bordered text-nowrap key-buttons">
                                 <thead>
@@ -29,62 +74,73 @@
                                         <th class="border-bottom-0" style="border: 1px solid black">New_Weight</th>
                                         <th class="border-bottom-0" style="border: 1px solid black">Price</th>
                                         <th class="border-bottom-0" style="border: 1px solid black">Buy_date</th>
-                                        <th class="border-bottom-0" style="border: 1px solid black">Sell_Date</th>
+                                        {{-- <th>Package</th> --}}
+                                        {{-- <th class="border-bottom-0">0.210-0.409</th>
+                                        <th class="border-bottom-0">0.410-5.000</th> --}}
+
+                                        <th class="border-bottom-0">Sell_Date</th>
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    @php
+                                        $count = 0;
+                                    @endphp
                                     @foreach ($inward as $key => $value)
 
+                                        @if ($value->s_id == 8)
 
-                                        <tr>
-                                            <td style="border: 1px solid black">
-                                                {{ $key + 1 }}
-                                            </td>
-                                            <td style="border: 1px solid black">
-                                                @php
-                                                    $s_id = $value->s_id;
-                                                    $s_name = App\Models\supplier_details::where('s_id', $s_id)->get('s_name');
-                                                    // echo $s_name;
-                                                @endphp
-                                                {{ $s_name[0]['s_name'] }}
-                                            </td>
-                                            <td style="border: 1px solid black">
-                                                {{ $value->d_barcode }}
-                                            </td>
-                                            <td style="border: 1px solid black">
-                                                @php
-                                                    $shape_id = $value->shape_id;
-                                                    $shape_name = App\Models\diamond_shape::where('shape_id', $shape_id)->get('shape_name');
-                                                    // echo $s_name;
-                                                @endphp
-                                                {{ $shape_name[0]['shape_name'] }}
-                                            </td>
-                                            <td style="border: 1px solid black">
-                                                {{ $value->d_wt }}
-                                            </td>
-                                            <td style="border: 1px solid black">
-                                                {{ $value->d_n_wt }}
-                                            </td>
-                                            <td style="border: 1px solid black">
-                                                {{ $value->price }}
-                                            </td>
-                                            <td style="border: 1px solid black">
-                                                {{ date('d-m-Y', strtotime($value->bill_date)) }}
-                                            </td>
-                                            <td style="border: 1px solid black">
-                                                {{ date('d-m-Y', strtotime($value->updated_at)) }}
-                                            </td>
+                                            @php
+                                                $count = $count + 1;
+                                            @endphp
+                                            <tr>
+                                                <td>
+                                                    {{ $key + 1 }}
+                                                </td>
+                                                <td>
+                                                    @php
+                                                        $s_id = $value->s_id;
+                                                        $s_name = App\Models\supplier_details::where('s_id', $s_id)->get('s_name');
+                                                        // echo $s_name;
+                                                    @endphp
+                                                    {{ $s_name[0]['s_name'] }}
+                                                </td>
+                                                <td>
+                                                    {{ $value->d_barcode }}
+                                                </td>
+                                                <td>
+                                                    @php
+                                                        $shape_id = $value->shape_id;
+                                                        $shape_name = App\Models\diamond_shape::where('shape_id', $shape_id)->get('shape_name');
+                                                        // echo $s_name;
+                                                    @endphp
+                                                    {{ $shape_name[0]['shape_name'] }}
+                                                </td>
+                                                <td>
+                                                    {{ $value->d_wt }}
+                                                </td>
+                                                <td>
+                                                    {{ $value->d_n_wt }}
+                                                </td>
+                                                <td>
+                                                    {{ $value->price }}
+                                                </td>
+                                                <td>
+                                                    {{ date('d-m-Y', strtotime($value->bill_date)) }}
+                                                </td>
+                                                <td>
+                                                    {{ date('d-m-Y', strtotime($value->updated_at)) }}
+                                                </td>
 
-                                        </tr>
+                                            </tr>
+                                        @endif
                                     @endforeach
                                 </tbody>
                             </table>
                         </div>
                     </div>
+                    <!--/div-->
                 </div>
             </div>
-            <!--/div-->
-
 
         </div>
     </div>
