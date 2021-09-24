d12bf87c592b62eabbae781d22e9536c3c043eba
Rushikesh Antala <86461351+RushikeshAntala@users.noreply.github.com>
2021-09-24 18:39:16 +0530

    try to supplier vise pdf genrater

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -39,19 +39,20 @@ class ReportController extends Controller
 
         return view('Report.Inward', $data);
     }
-    public function genratePDF_Inward()
+    public function genratePDF_Inward(Request $request)
     {
+        $s_id = $request->s_id;
         $data = array();
         $c_id = session()->get('c_id');
         $data['inward'] = D_Purchase::where('c_id', $c_id)->get();
         $data['inward'] = D_Purchase::join('supplier_details', 'd_purchase.s_id', '=', 'supplier_details.s_id')
             ->join('diamond_shape', 'd_purchase.shape_id', '=', 'diamond_shape.shape_id')
-            ->where([['supplier_details.c_id', $c_id]])
+            ->where([['supplier_details.c_id', $c_id], ['d_purchase.s_id', $s_id]])
             ->get(['d_purchase.*', 'supplier_details.*', 'diamond_shape.*']);
+        echo $s_id;
+        // $pdf = PDF::loadView('Report.Inward_Formatte', $data);
 
-        $pdf = PDF::loadView('Report.Inward_Formatte', $data);
-
-        return $pdf->download('Inward.pdf');
+        // return $pdf->download('Inward.pdf');
     }
     public function search_data_Inward(Request $request)
     {
