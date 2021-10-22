fdbce615f3c067ee15132b56aa484b41621039e7
Rushikesh Antala <86461351+RushikeshAntala@users.noreply.github.com>
2021-10-22 18:52:58 +0530

    update database

--- app/Http/Controllers/DiamondController.php
+++ app/Http/Controllers/DiamondController.php
@@ -47,19 +47,19 @@ class DiamondController extends Controller
 
         $s_id = $request->s_id;
         $d_wt = $request->d_wt;
+
+        $validator = Validator::make($request->all(), [
+            'bar_code' => 'required',
+            'd_wt' => 'required',
+            'shape_id' => 'required',
+            's_id' => 'required',
+            'bill_date' => 'required'
+
+        ]);            //dd($request);
+        if ($validator->fails()) {
+            return Response::json(array('success' => false));
+        }
         try {
-            $validator = Validator::make($request->all(), [
-                'bar_code' => 'required',
-                'd_wt' => 'required',
-                'shape_id' => 'required',
-                's_id' => 'required',
-                'bill_date' => 'required'
-
-            ]);            //dd($request);
-            if ($validator->fails()) {
-                return Response::json(array('success' => false));
-            }
-
             $barcodeExist = D_Purchase::where('d_barcode', $request->bar_code)->first();
 
             if ($barcodeExist == null) {
@@ -75,31 +75,36 @@ class DiamondController extends Controller
                 //$newitem->d_col = !empty($request->d_col) ? $request->d_col : '';
                 //$newitem->d_pc = !empty($request->d_pc) ? $request->d_pc : '';
                 $newitem->shape_id = !empty($request->shape_id) ? $request->shape_id : '';
-                $json_data = rate_master::where('rate_masters.s_id', $s_id)->get();
-                $json_decoded = json_decode($json_data[0]['json_price']);
-                foreach ($json_decoded[0] as $key => $val) {
-                    $r_id = $key;
-                    $wt_category = rate::where('rates.r_id', $r_id)->get();
-                    $wt_category = $wt_category[0]['wt_category'];
-                    $value = explode('-', $wt_category);
-                    // $first_value = substr($wt_category, 0, 5);
-                    // $last_value = substr($wt_category, -5);
-                    if ($value[0] <= $d_wt && $value[1] >= $d_wt) {
-                        $newitem->d_wt_category = $key;
-                        $newitem->price = $val;
+                try {
+                    $json_data = rate_master::where('rate_masters.s_id', $s_id)->first();
+                    $json_decoded = json_decode($json_data['json_price']);
+                    foreach ($json_decoded[0] as $key => $val) {
+                        $r_id = $key;
+                        $wt_category = rate::where('rates.r_id', $r_id)->first();
+                        $wt_category = $wt_category['wt_category'];
+                        $value = explode('-', $wt_category);
+                        // $first_value = substr($wt_category, 0, 5);
+                        // $last_value = substr($wt_category, -5);
+                        if ($value[0] <= $d_wt && $value[1] >= $d_wt) {
+                            $newitem->d_wt_category = $key;
+                            $newitem->price = $val;
+                            break;
+                        }
                     }
+                } catch (\Throwable $th) {
+                    return Response::json(array('success' => 311));
                 }
                 //$newitem->d_cla = !empty($request->d_cla) ? $request->d_cla : '';
                 //$newitem->d_exp_pr = !empty($request->d_exp_pr) ? $request->d_exp_pr : '';
                 //$newitem->d_exp = !empty($request->d_exp) ? $request->d_exp : '';
                 $newitem->save();
                 // dd($newitem);
-                return Response::json(array('success' => true));
-            } else {
                 return Response::json(array('success' => 200));
+            } else {
+                return Response::json(array('success' => 312));
             }
         } catch (\Throwable $th) {
-            return Response::json(array('success' => false));
+            return Response::json(array('success' => 408));
         }
     }
     public function edit(Request $request)
--- db/vm_jewel.sql
+++ db/vm_jewel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 21, 2021 at 03:10 PM
+-- Generation Time: Oct 22, 2021 at 03:20 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
